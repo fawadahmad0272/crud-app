42470c7ff2de01813ed474b57ab82eeaa15f95ae
Fawad Ahmad <fawadahmad0272@gmail.com>
2025-11-06 10:29:48 +0500

    Initial commit - CRUD app in ASP.NET MVC

---
+++ CrudApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CrudApp/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CrudApp.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display (Name ="Student ID")]
+        public int StudentId { get; set; }
+        [Required]
+        [Display(Name ="Student Name")]
+        public string StudentName { get; set; }
+        [Required]
+        [Display(Name ="Email")]
+        public string StudentEmail { get; set; }
+        [Required]
+        [Display(Name ="Course")]
+        public string Course { get; set; }
+        [Required]
+        [Display(Name ="Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        [Required]
+        [Display(Name ="Is Active?")]
+        public bool IsActive { get; set; }
+
+    }
+}
